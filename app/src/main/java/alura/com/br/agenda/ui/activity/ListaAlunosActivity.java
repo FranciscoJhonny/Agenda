0761ac12e4f24746fdb0efe438d4d18ea37e3fb5
Francisco Jhonny <francisco.jhonny@hotmail.com>
2022-11-14 17:42:58 -0300

    Aula 05 Criando a activity via android studio

--- app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,8 +1,9 @@
-package alura.com.br.agenda;
+package alura.com.br.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +11,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import alura.com.br.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Lista de Alunos");
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Francisco","Kassiany","Patrícia","Kevin Jhonny"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
