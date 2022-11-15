44b33d66ee73a926d69ccc5be9cbcf77733d45e0
Francisco Jhonny <francisco.jhonny@hotmail.com>
2022-11-15 10:54:51 -0300

    05 - Finalizando o fluxo do App - Aula 05 - Finalizando fluxo do App

--- app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,15 +1,16 @@
 package alura.com.br.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import alura.com.br.agenda.R;
 import alura.com.br.agenda.dao.AlunoDAO;
@@ -19,12 +20,25 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Lista de Alunos");
         setContentView(R.layout.activity_lista_alunos);
 
+        setTitle("Lista de Alunos");
+
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+    botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(ListaAlunosActivity.this,FormularioAlunoActivity.class));
+        }
+    });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         AlunoDAO dao = new AlunoDAO();
-
-
 
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
